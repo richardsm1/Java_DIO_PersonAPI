a80b9dcd1e861682f4dea37e258a072c21c987b9
Ricardo Murca <richard_sm1@outlook.com>
2021-07-22 18:44:01 -0300

    Criada a classe MessageResponseDTO, a interface PersonRepository e implementado o método createPerson na clase PersonController

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,7 +1,11 @@
 package one.digitalinnovation.personapi.controller;
 
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
-import org.springframework.web.bind.annotation.GetMapping;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBooking(){
+    private final PersonRepository personRepository;
 
-        return "API Rest";
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
     }
 }
